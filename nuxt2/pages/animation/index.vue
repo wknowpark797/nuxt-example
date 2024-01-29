3f05be1ee6df23ccbba0eb6d3b50eacf87a1d5f6
wknowpark797 <104073140+wknowpark797@users.noreply.github.com>
2024-01-29 17:55:36 +0900

    [nuxt2] setInterval 사용 비교

--- nuxt2/pages/animation/index.vue
+++ nuxt2/pages/animation/index.vue
@@ -1,13 +1,13 @@
-// 애니메이션 관련 최적화 API - requestAnimationFrame()
+// 애니메이션 관련 최적화 API - requestAnimationFrame() 사용예제
 
 <template>
     <div>
         <div ref="box" class="box" />
-        position: <span ref="value">0</span>
-
-        <button type="button" @click="onPause">
-            PAUSE
-        </button>
+        <div ref="box2" class="box" />
+        
+        <button type="button" @click="onPause">일시정지</button>
+        <p>Position : <span ref="value">0</span></p>
+        <p>애니메이션 수행 시간 : {{ currentTime }}</p>
     </div>
 </template>
 
@@ -18,17 +18,24 @@ export default {
         return {
             xPos: 0,
             animationId: 0,
+            animationId2: 0,
             startTime: 0,   // 애니메이션 시작 시간
+            currentTime: 0, // 애니메이션 수행 시간
         }
     },
     methods: {
+        // requestAnimationFrame 사용
         render(timestamp) {
             if(!this.startTime) this.startTime = timestamp;
-            const currentTime = timestamp - this.startTime;
-            console.log('애니메이션 수행 시간: ', currentTime);
-
+            this.currentTime = timestamp - this.startTime;
+            
             // 5초 후 정지
-            if(currentTime > 5000) {
+            // if(this.currentTime > 5000) {
+            //     cancelAnimationFrame(this.animationId);
+            //     return;
+            // }
+
+            if(this.xPos > 500) {
                 cancelAnimationFrame(this.animationId);
                 return;
             }
@@ -39,6 +46,17 @@ export default {
 
             this.animationId = requestAnimationFrame(this.render);
         },
+
+        // setInterval 사용
+        render2() {
+            if(this.xPos > 500) {
+                clearInterval(this.animationId2);
+                return;
+            }
+            this.$refs.box2.style.transform = `translateX(${this.xPos}px)`;
+        },
+
+        // 일시 정지
         onPause() {
             cancelAnimationFrame(this.animationId);
 
@@ -48,8 +66,8 @@ export default {
         }
     },
     mounted() {
-        // this.render();
         requestAnimationFrame(this.render);
+        this.animationId2 = setInterval(this.render2, 1000/60);
     }
 }
 </script>
