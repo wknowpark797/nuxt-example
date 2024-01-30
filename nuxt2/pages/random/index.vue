8f595a1801ce067ac3de64d650e0968a2299784e
wknowpark797 <104073140+wknowpark797@users.noreply.github.com>
2024-01-30 10:54:27 +0900

    [nuxt2] random 페이지 마크업 수정

--- nuxt2/pages/random/index.vue
+++ nuxt2/pages/random/index.vue
@@ -1,9 +1,25 @@
 <template>
     <div>
-        <p>Random</p>
-        {{ data }}
+        <h1>Category</h1>
 
-        <div style="height: 1000px" />
+        <ul class="category-list">
+            <li 
+                v-for="item in categories" 
+                :key="item.idCategory">
+
+                <div class="box">
+                    <div class="title-wrap">
+                        <p>{{ item.strCategory }}</p>
+
+                        <div class="thumb">
+                            <img :src="item.strCategoryThumb" :alt="item.strCategory">
+                        </div>
+                    </div>
+                    
+                    <p class="desc">{{ item.strCategoryDescription }}</p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -25,8 +41,52 @@ export default {
 
     // Axios
 	async asyncData({ $axios }) {
-		const data = await $axios.$get('https://www.themealdb.com/api/json/v1/1/random.php');
-		return { data };
+		const response = await $axios.$get('https://www.themealdb.com/api/json/v1/1/categories.php');
+		return { categories: response.categories };
 	},
 }
 </script>
+
+<style lang="scss" scoped>
+    .category-list {
+        li {
+            margin: 30px 0;
+
+            &:nth-child(even) {
+                .box {
+                    margin-left: auto;
+                }
+            }
+
+            .box {
+                width: 350px;
+                border: 1px solid #333;
+                padding: 10px;
+
+                .title-wrap {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    margin-bottom: 10px;
+
+                    .thumb {
+                        width: 150px;
+
+                        img {
+                            width: 100%;
+                            object-fit: cover;
+                        }
+                    }
+                }
+                
+                .desc {
+                    white-space: normal;
+                    display: -webkit-box;
+                    -webkit-line-clamp: 3;
+                    -webkit-box-orient: vertical;
+                    overflow: hidden;
+                }
+            }
+        }
+    }
+</style>
